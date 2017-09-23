dc97ac9a9e317746cb169c18a6af3d8d25bb70fe
Karen Farbman <karen.farbman@gmail.com>
2017-09-23 17:43:51 -0600

    add app before model

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ with open('2017_dict.pkl','rb') as f:
 
 autoclaves = ['150','250','350','450']
 agitators = ['A','B','C','D','E','F','G']
-features = ['Vibration','Cooling Water','Temperature','Compartment Temperature']
+features = ['vibration','coolingWater','temperature','compartmentTemperature']
 
 tree = lambda: defaultdict(tree)
 dct = tree()
@@ -24,11 +24,21 @@ for autoclave in autoclaves:
     for agitator in agitators:
         for feature, i in zip(features,range(3,7)):
             dct[autoclave]['agitators'][agitator][feature] = test_dict[autoclave][agitator][test_dict[autoclave][agitator].columns[i]][-1]
-dct['150']['up_date'] = '2016-11-23'
-dct['250']['up_date'] = '2017-02-12'
-dct['350']['up_date'] = '2016-11-23'
-dct['450']['up_date'] = '2017-02-08'
-                            
+        scaled = ss.transform(test_dict[autoclave][agitators].values)
+        prediction = model.predict(scaled)
+        dct[autoclave]['agitators'][agitator]['daysToFailure'] = prediction
+dct['150']['upDate'] = '2016-11-23'
+dct['250']['upDate'] = '2017-02-12'
+dct['350']['upDate'] = '2016-11-23'
+dct['450']['upDate'] = '2017-02-08'
+
+
+
+for autoclave in autoclaves:
+    for agitator in agitators:
+        scaled = ss.transform(test_dict[autoclave][agitators].values)
+        prediction = model.predict(scaled)
+        dct[autoclave]['agitators'][agitator]['daysToFailure'] = prediction
 
     
     
