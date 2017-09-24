dd1272758af7089bb94c52785b42bbdc2bd1ca88
Anne Morrow <anne.moore2008@gmail.com>
2017-09-24 11:00:43 -0600

    target_creator.py updated to make dummie collumns

--- target_creator.py
+++ target_creator.py
@@ -26,8 +26,12 @@ def days_to_failure(row_date, fail_date_list):
 def limit_dataframe(data, autoclave, agitator):
     autoclave_cols = ['Autoclave Total Feed', 'Autoclave Level',
        'Autoclave Vent Valve Position', 'Autoclave Pressure']
-    agitator_cols = [col for col in data.columns if ' {} '.format(agitator) in col]
-    agitator_data = data.loc[data.AUT == autoclave, autoclave_cols + agitator_cols]
+    bad_cols = [col for word in ['Current', 'Power', 'O2', 'Speed']
+                for col in data.columns if word in str(col)]
+    dummy_cols = [150, 250, 350]
+    agitator_data = data.drop(bad_cols, axis=1)
+    agitator_cols = [col for col in agitator_data.columns if ' {} '.format(agitator) in str(col)]
+    agitator_data = data.loc[data.AUT == autoclave, autoclave_cols + agitator_cols + dummy_cols]
     return agitator_data
 
 def create_targeted(data, autoclave, agitator, fail_date_list):
@@ -36,8 +40,30 @@ def create_targeted(data, autoclave, agitator, fail_date_list):
     target = agitator_data.index.to_series().apply(
         lambda d: days_to_failure(d, fail_date_list))
     agitator_data['target'] = target
+    agitator_data.columns = ['Autoclave Total Feed', 'Autoclave Level',
+       'Autoclave Vent Valve Position', 'Autoclave Pressure',
+       'Agitator Vibration', 'Agitator Cooling Water',
+       'Agitator temperature', 'Autoclave Compartment temperature', 150, 250, 350,
+       'target']
     return agitator_data
 
 if __name__ == '__main__':
     data = pd.read_pickle('numeric_data.pkl')
-    targeted_data = create_targeted(data, 350, 'C', [datetime.datetime(2015, 11, 3)])
+    dummies = pd.get_dummies(data['AUT']).drop(450, axis=1)
+    data = data.merge(dummies, right_index=True, left_index=True)
+
+    B_350 = create_targeted(data, 350, 'B', [datetime.datetime(2014, 4, 25)])
+    C_350 = create_targeted(data, 350, 'C', [datetime.datetime(2015, 11, 3),
+                                             datetime.datetime(2016, 11, 7)])
+    D_150 = create_targeted(data, 150, 'D', [datetime.datetime(2015, 4, 6)])
+    D_250 = create_targeted(data, 250, 'D', [datetime.datetime(2014, 1, 7)])
+    D_450 = create_targeted(data, 450, 'D', [datetime.datetime(2015, 11, 27)])
+
+    all_together_now = pd.concat([B_350, C_350, D_150, D_250, D_450])
+    all_together_now.to_pickle('all_together_now.pkl')
+
+    # B_350.to_pickle('B_350.pkl')
+    # C_350.to_pickle('C_350.pkl')
+    # D_150.to_pickle('D_150.pkl')
+    # D_250.to_pickle('D_250.pkl')
+    # D_450.to_pickle('D_450.pkl')
